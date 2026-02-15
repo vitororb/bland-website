987add43248c3e6b43c466e6f4e3577ab63fa9e2
VitorORBrandao <vitororesendeb@gmail.com>
2026-02-15 19:29:22 -0400

    feat: adiciona componente de Divisor

---
+++ components/shared/Divisor.tsx
@@ -0,0 +1,7 @@
+export const Divisor = () => {
+  return (
+    <div className="flex w-full items-center justify-center my-8">
+      <img src="/divisor-bland.png" alt="Divisor" className="h-10" />
+    </div>
+  );
+};
