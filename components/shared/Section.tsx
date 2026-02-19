97961ffbc6be75cf7989e76156412133af1cb3fb
VitorORBrandao <vitororesendeb@gmail.com>
2026-02-18 21:33:35 -0400

    fix: ajusta a formatação do componente Section para melhorar a estrutura do layout

--- components/shared/Section.tsx
+++ components/shared/Section.tsx
@@ -12,12 +12,14 @@ export const Section = ({ id, title, subtitle, children }: Props) => {
       className="my-12 flex w-full flex-col items-center gap-12 px-8 text-neutral-900"
     >
       <div className="flex max-w-3xl flex-col gap-8 text-center">
-        <h1 className="text-secondary font-family-cinzel text-5xl font-bold">
+        <h1 className="text-secondary font-family-cinzel text-4xl font-bold">
           {title}
         </h1>
         {subtitle && <p className="mb-4 text-lg text-gray-600">{subtitle}</p>}
       </div>
-      {children}
+      <div className="mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8">
+        {children}
+      </div>
     </div>
   );
 };
