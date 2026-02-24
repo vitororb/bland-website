691b43548b13d386f9a5c636c06ebeee57ae3c9c
vitororb <vitororesendeb@gmail.com>
2026-02-23 23:49:46 -0400

    fix: reorganiza a estrutura do componente Navbar para melhor legibilidade e adiciona responsividade

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -14,33 +14,42 @@ export const Navbar = () => {
   ];
 
   return (
-    <nav className="fixed top-0 z-50 flex h-20 w-screen items-center justify-between bg-white/60 px-4 shadow-lg backdrop-blur-sm">
-      {/* Logo */}
-      <Link href="/" className="hover:cursor-pointer">
-        <img
-          src="/logos/logo-h.png"
-          alt="Bland Motel"
-          className="h-20 w-auto"
-        />
-      </Link>
+    <nav className="fixed top-0 z-50 flex w-screen flex-col bg-white/60 px-4 shadow-lg backdrop-blur-sm">
+      <div className="flex w-full items-center justify-between">
+        {/* Logo */}
+        <Link href="/" className="hover:cursor-pointer">
+          <img
+            src="/logos/logo-h.png"
+            alt="Bland Motel"
+            className="h-20 w-auto"
+          />
+        </Link>
 
-      {/* Menu */}
-      <div className="flex items-center gap-8">
+        {/* Menu */}
+        <div className="hidden items-center gap-8 md:flex">
+          {navItems.map((item) => (
+            <NavItem key={item.id} url={`/${item.id}`} text={item.label} />
+          ))}
+        </div>
+
+        {/* Contact Button */}
+        <a
+          href="https://wa.me/5564999500479"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-primary hover:bg-secondary flex items-center rounded-lg px-4 py-2 text-white transition-colors"
+          title="WhatsApp"
+        >
+          Reserve
+        </a>
+      </div>
+
+      {/* Mobile Menu Aberto*/}
+      <div className="flex items-center gap-8 overflow-x-auto border-t border-gray-200 p-4 md:hidden">
         {navItems.map((item) => (
           <NavItem key={item.id} url={`/${item.id}`} text={item.label} />
         ))}
       </div>
-
-      {/* Contact Button */}
-      <a
-        href="https://wa.me/5564999500479"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="bg-primary hover:bg-secondary flex items-center rounded-lg px-4 py-2 text-white transition-colors"
-        title="WhatsApp"
-      >
-        Reserve
-      </a>
     </nav>
   );
 };
