614b6f339e772107ae22e3cc41da905c84ac8459
vitororb <vitororesendeb@gmail.com>
2026-08-21 11:52:25 -0400

    feat: melhora a estilização da página 404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,21 +1,27 @@
 import { Button } from "@/components/Button";
+import { ArrowRightIcon } from "lucide-react";
 
 export default function NotFound() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center px-4">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="font-cinzel text-foreground text-9xl font-bold">404</h1>
+    <div className="flex min-h-screen flex-col items-center justify-center gap-4 px-6 text-center">
+      <h1 className="font-family-cinzel text-foreground text-9xl font-bold">
+        404
+      </h1>
 
-        <p className="text-foreground mt-4 text-2xl font-semibold">
-          Página não encontrada
-        </p>
+      <p className="text-foreground text-2xl font-semibold">
+        Página não encontrada
+      </p>
 
-        <p className="text-foreground/70 mt-2">
-          A página que você está procurando não existe.
-        </p>
+      <p className="text-foreground/70">
+        A página que você está procurando não existe.
+      </p>
 
-        <Button label="Voltar para o início" href="/" className="mt-6 w-fit" />
-      </div>
+      <Button
+        label="Voltar para o Início"
+        href="/"
+        className="w-fit"
+        rightIcon={<ArrowRightIcon size={16} />}
+      />
     </div>
   );
 }
